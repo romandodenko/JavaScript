c8c9ccc0df055bdab3371e4237a2ab3cc4374a15
Roman Dodenko <creed6695@mail.ru>
2022-11-07 21:03:51 +0700

    Initial commit

---
+++ TODO/script.js
@@ -0,0 +1,14 @@
+const button = document.querySelector(".app__button");
+
+button.addEventListener("click", function () {
+  const input = document.querySelector(".app__input");
+  const taskMessage = input.value;
+  input.value = "";
+
+  const task = `<li class="app__item">${taskMessage}</li>`;
+  const list = document.querySelector(".app__list");
+  list.innerHTML += task;
+
+  const counter = document.querySelector(".counter");
+  counter.innerHTML = list.querySelectorAll(".app__item").length;
+})
